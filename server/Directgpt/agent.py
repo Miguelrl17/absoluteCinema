4f5247ad22c84638b05bffcb8e142b8987c5e32c
desbyee <[DestinyGonzalez2904@gmail.com]>
2025-04-19 16:21:58 -0700

    added simple flask logic and git ignore

--- server/Directgpt/agent.py
+++ server/Directgpt/agent.py
@@ -7,16 +7,10 @@ load_dotenv()
 
 # Access the API key
 GEMINI = os.getenv("GEMINI_API_KEY")
+
 client = genai.Client(api_key= GEMINI)
-global reccPrompt
 
-
-with open("server/Directgpt/reccPrompt.txt", "r") as file:
-    reccPrompt = file.read()
-
-
-
-def getReccs(json):
-    response = client.models.generate_content(
-        model="gemini-2.0-flash", contents= f"{reccPrompt} {json}")
-    return response.text
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+)
+print(response.text)
--- server/server.py
+++ server/server.py
@@ -1,23 +1,3 @@
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
-from .Directgpt.agent import Agent
 
-app = Flask(__name__)
-
-@app.route('/reccs', methods=['GET'])
-def ask_model():
-    #input_data = request.args.get('data')
-    #if not input_data:
-        #return jsonify({"error": "Missing 'data' query parameter"}), 400
-    try:
-        input_data ={"Age":18, "Rating": "G", "sensors":"strobing lights","genere": "action"}
-        response = Agent.getReccs(input_data)
-        if not response:
-            return jsonify({"error": "No response from model"}), 500
-        return jsonify({"response": response.text})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-# Run the server
-if __name__ == '__main__':
-    app.run(debug=True)
