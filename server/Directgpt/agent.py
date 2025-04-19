36303098ba1746bde9a599141e91d3a82253c608
desbyee <[DestinyGonzalez2904@gmail.com]>
2025-04-19 16:18:57 -0700

    added simple flask server logic

--- server/Directgpt/agent.py
+++ server/Directgpt/agent.py
@@ -7,10 +7,16 @@ load_dotenv()
 
 # Access the API key
 GEMINI = os.getenv("GEMINI_API_KEY")
-
 client = genai.Client(api_key= GEMINI)
+global reccPrompt
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
+
+with open("server/Directgpt/reccPrompt.txt", "r") as file:
+    reccPrompt = file.read()
+
+
+
+def getReccs(json):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents= f"{reccPrompt} {json}")
+    return response.text
